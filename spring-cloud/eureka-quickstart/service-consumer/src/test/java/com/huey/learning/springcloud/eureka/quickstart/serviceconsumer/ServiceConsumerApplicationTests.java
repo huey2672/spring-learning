5d48de101658a8cc0804c9d1671285ad446925d5
huey <huey2672@163.com>
2020-06-12 00:14:13 +0800

    spring cloud eureka quick start

---
+++ spring-cloud/eureka-quickstart/service-consumer/src/test/java/com/huey/learning/springcloud/eureka/quickstart/serviceconsumer/ServiceConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huey.learning.springcloud.eureka.quickstart.serviceconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
