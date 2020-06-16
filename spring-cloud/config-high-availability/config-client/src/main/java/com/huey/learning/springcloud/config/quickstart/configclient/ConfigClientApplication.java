74c8971fa45d86a6fc00311fb7b39835edca1c29
huey <huey2672@163.com>
2020-06-17 00:15:14 +0800

    config high availability

--- spring-cloud/config-high-availability/config-client/src/main/java/com/huey/learning/springcloud/config/quickstart/configclient/ConfigClientApplication.java
+++ spring-cloud/config-high-availability/config-client/src/main/java/com/huey/learning/springcloud/config/quickstart/configclient/ConfigClientApplication.java
@@ -1,11 +1,11 @@
 package com.huey.learning.springcloud.config.quickstart.configclient;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@EnableDiscoveryClient
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
