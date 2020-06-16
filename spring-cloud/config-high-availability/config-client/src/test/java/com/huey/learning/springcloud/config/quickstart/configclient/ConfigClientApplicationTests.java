1379a8c716aa99be48cc221fb154963db60beb71
huey <huey2672@163.com>
2020-06-16 23:38:05 +0800

    refactor

---
+++ spring-cloud/config-high-availability/config-client/src/test/java/com/huey/learning/springcloud/config/quickstart/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huey.learning.springcloud.config.quickstart.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
