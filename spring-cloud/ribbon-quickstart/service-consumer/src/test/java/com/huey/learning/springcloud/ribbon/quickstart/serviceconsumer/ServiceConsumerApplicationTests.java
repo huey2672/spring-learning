82ad4de3ddc8a1130ef2cb7e49608a3536814d9c
huey <huey2672@163.com>
2020-06-18 01:01:48 +0800

    Ribbon quick start

---
+++ spring-cloud/ribbon-quickstart/service-consumer/src/test/java/com/huey/learning/springcloud/ribbon/quickstart/serviceconsumer/ServiceConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huey.learning.springcloud.ribbon.quickstart.serviceconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
