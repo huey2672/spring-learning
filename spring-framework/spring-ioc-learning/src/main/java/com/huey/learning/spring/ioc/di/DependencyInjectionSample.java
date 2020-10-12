a26e319a21b00f6f0a3456f60052f2a390802085
huey <huey2672@163.com>
2020-10-12 16:58:57 +0800

    dependency injection

---
+++ spring-framework/spring-ioc-learning/src/main/java/com/huey/learning/spring/ioc/di/DependencyInjectionSample.java
@@ -0,0 +1,23 @@
+package com.huey.learning.spring.ioc.di;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author huey
+ */
+public class DependencyInjectionSample {
+
+    public static void main(String[] args) throws Exception {
+
+        AbstractApplicationContext appCtx = new ClassPathXmlApplicationContext("dependency-injection/spring.xml");
+        appCtx.start();
+
+        Weapon bow = appCtx.getBean("bow", Weapon.class);
+        System.out.println(bow);
+
+        appCtx.stop();
+
+    }
+
+}
---
+++ spring-framework/spring-ioc-learning/src/main/java/com/huey/learning/spring/ioc/di/Weapon.java
@@ -0,0 +1,44 @@
+package com.huey.learning.spring.ioc.di;
+
+import java.beans.ConstructorProperties;
+
+/**
+ * @author huey
+ */
+public class Weapon {
+
+    private String type;
+    private Double weightKg;
+
+    public Weapon() {
+        super();
+    }
+
+    @ConstructorProperties({"type", "weightKg"})
+    public Weapon(String type, Double weightKg) {
+        this.type = type;
+        this.weightKg = weightKg;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getWeightKg() {
+        return weightKg;
+    }
+
+    public void setWeightKg(Double weightKg) {
+        this.weightKg = weightKg;
+    }
+
+    @Override
+    public String toString() {
+        return "Weapon{" + "type='" + type + '\'' + ", weightKg=" + weightKg + '}';
+    }
+
+}
