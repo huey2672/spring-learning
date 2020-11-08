0d10e94ff7f92e3ba21119fcdba8230d225f6426
huey <huey2672@163.com>
2020-11-08 20:06:13 +0800

    refactor

--- spring-framework/spring-ioc-learning/src/main/java/com/huey/learning/spring/ioc/importannotation/DaoConfiguration.java
+++ spring-framework/spring-ioc-learning/src/main/java/com/huey/learning/spring/ioc/importannotation/DaoConfiguration.java
@@ -1,10 +1,7 @@
 package com.huey.learning.spring.ioc.importannotation;
 
-import org.springframework.context.annotation.Configuration;
-
 /**
  * @author huey
  */
-@Configuration
 public class DaoConfiguration {
 }
