22dac8dbec232765b2f7085fb97507c5bbe8263e
huey <huey2672@163.com>
2020-12-31 15:45:38 +0800

    springboot properties with maven

---
+++ spring-boot/springboot-with-properties/src/main/java/com/huey/learning/springboot/quickstart/properties/maven/MavenPlaceholderController.java
@@ -0,0 +1,26 @@
+package com.huey.learning.springboot.quickstart.properties.maven;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * @author huey
+ */
+@RestController
+@ConfigurationProperties("maven")
+@Data
+public class MavenPlaceholderController {
+
+    private Map<String, String> properties;
+
+    @GetMapping("maven/properties")
+    public Map<String, String> mavenProperties() {
+        return properties;
+    }
+
+}
