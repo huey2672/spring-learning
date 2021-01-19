f93025e92200c43bde3305ab9560cd02c3d2781e
huey <huey2672@163.com>
2021-01-19 20:57:54 +0800

    spring boot actuator

---
+++ spring-boot/actuator-quickstart/src/main/java/com/huey/learning/springboot/actuator/quickstart/ActuatorQuickstartApplication.java
@@ -0,0 +1,13 @@
+package com.huey.learning.springboot.actuator.quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorQuickstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorQuickstartApplication.class, args);
+    }
+
+}
---
+++ spring-boot/actuator-quickstart/src/test/java/com/huey/learning/springboot/actuator/quickstart/ActuatorQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huey.learning.springboot.actuator.quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorQuickstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
