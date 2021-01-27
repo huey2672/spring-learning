a94781e6cb433bc91c16498f1426f2cd0d2c1193
huey <huey2672@163.com>
2021-01-27 10:18:44 +0800

    spring boot

--- spring-boot/springboot-with-properties/src/main/java/com/huey/learning/springboot/quickstart/properties/maven/MavenPlaceholderController.java
+++ spring-boot/springboot-with-properties/src/main/java/com/huey/learning/springboot/quickstart/properties/maven/MavenPlaceholderController.java
@@ -1,7 +1,6 @@
 package com.huey.learning.springboot.quickstart.properties.maven;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
