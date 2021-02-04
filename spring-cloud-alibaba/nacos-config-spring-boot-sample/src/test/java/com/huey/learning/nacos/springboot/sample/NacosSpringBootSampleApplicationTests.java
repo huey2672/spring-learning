a231eb1b5cc21a361816fede3558395adce21dd5
huey <huey2672@163.com>
2021-02-04 16:37:18 +0800

    nacos quick start

---
+++ spring-cloud-alibaba/nacos-config-spring-boot-sample/src/test/java/com/huey/learning/nacos/springboot/sample/NacosSpringBootSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huey.learning.nacos.springboot.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosSpringBootSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
