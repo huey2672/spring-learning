b906ea7407a18ff4e4d61dc96eea9ce951358719
huey2672 <huey2672@163.com>
2021-07-21 19:40:45 +0800

    spring cloud with nacos config

---
+++ spring-cloud-alibaba/nacos-config-spring-cloud-sample/src/main/java/com/huey/learning/nacos/config/springcloud/sample/NacosConfigSpringCloudSampleApplication.java
@@ -0,0 +1,13 @@
+package com.huey.learning.nacos.config.springcloud.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosConfigSpringCloudSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigSpringCloudSampleApplication.class, args);
+    }
+
+}
