943d1b03af04ff7575c1c5fc245ed6d0ae3e9f6e
huey2672 <huey2672@163.com>
2022-03-31 14:43:28 +0800

    actuator & prometheus

---
+++ spring-boot/actuator-prometheus/src/main/java/com/huey/learning/springboot/actuator/prometheus/ActuatorPrometheusApplication.java
@@ -0,0 +1,13 @@
+package com.huey.learning.springboot.actuator.prometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorPrometheusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorPrometheusApplication.class, args);
+    }
+
+}
