10b6c6c3d9d00ce2be4341aa701cd157b48a70b5
zheng.zhisen <zheng.zhisen@iwhalecloud.com>
2023-08-20 00:21:31 +0800

    spring cloud gateway quick start

---
+++ spring-cloud/gateway-quickstart/src/main/java/com/example/gatewayquickstart/GatewayQuickstartApplication.java
@@ -0,0 +1,30 @@
+package com.example.gatewayquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayQuickstartApplication {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_route",
+                        r -> r.path("/example")
+                                .uri("http://example.com")
+                )
+                .route("query_route",
+                        r -> r.query("foo", "Foo")
+                                .filters(f -> f.addResponseHeader("X-Bar", "Bar"))
+                                .uri("http://www.baeldung.com/"))
+                .build();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayQuickstartApplication.class, args);
+    }
+
+}
---
+++ spring-cloud/gateway-quickstart/src/test/java/com/example/gatewayquickstart/GatewayQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewayquickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayQuickstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
