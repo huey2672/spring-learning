35da1e3a125286dfe7a33a01dfa0d4d0242d8841
zheng.zhisen <zheng.zhisen@iwhalecloud.com>
2023-08-23 15:42:37 +0800

    zipkin sample

---
+++ spring-cloud/sleuth-zipkin-sample/service-consumer/src/main/java/com/huey/learning/springcloud/sleuth/zipkin/service/consumer/HelloController.java
@@ -0,0 +1,23 @@
+package com.huey.learning.springcloud.sleuth.zipkin.service.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@RestController
+public class HelloController {
+    
+    @Resource
+    private HelloRemote helloRemote;
+
+    @RequestMapping("/hello/{name}")
+    public String hello(@PathVariable("name") String name) {
+        log.info("Hello, {}!", name);
+        return helloRemote.hello(name);
+    }
+    
+}
