acba8a75ad3685c3dcd2a60ff8f34ab24ced4e94
zheng.zhisen <zheng.zhisen@iwhalecloud.com>
2023-09-06 23:20:30 +0800

    zipkin rabbitmq

---
+++ spring-cloud/zipkin-rabbitmq-sample/service-provider/src/test/java/com/huey/learning/springcloud/sleuth/zipkin/rabbitmq/service/provider/ServiceProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huey.learning.springcloud.sleuth.zipkin.rabbitmq.service.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
