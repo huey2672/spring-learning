7518ff1a1f1343139ee975ef8d365eeb5c47ad87
zheng.zhisen <zheng.zhisen@iwhalecloud.com>
2023-09-06 23:20:15 +0800

    zipkin rabbitmq

---
+++ spring-cloud/zipkin-rabbitmq-sample/service-consumer/src/main/java/com/huey/learning/springcloud/sleuth/zipkin/rabbitmq/service/consumer/HelloRemote.java
@@ -0,0 +1,13 @@
+package com.huey.learning.springcloud.sleuth.zipkin.rabbitmq.service.consumer;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "my-service-provider")
+public interface HelloRemote {
+    
+    @RequestMapping("/hello")
+    String hello(@RequestParam("name") String name);
+    
+}
---
+++ spring-cloud/zipkin-rabbitmq-sample/service-consumer/src/main/java/com/huey/learning/springcloud/sleuth/zipkin/rabbitmq/service/consumer/ServiceConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.huey.learning.springcloud.sleuth.zipkin.rabbitmq.service.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class ServiceConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceConsumerApplication.class, args);
+    }
+
+}
