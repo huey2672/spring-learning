220d011c578a259003c5dfec8a9dfac5af3996d6
huey2672 <huey2672@163.com>
2024-01-15 16:07:03 +0800

    logging

---
+++ spring-boot/logging/src/main/java/com/huey/learning/springboot/logging/SpringbootWithLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.huey.learning.springboot.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithLoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWithLoggingApplication.class, args);
+    }
+
+}
---
+++ spring-boot/logging/src/test/java/com/huey/learning/springboot/logging/SpringbootWithLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huey.learning.springboot.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithLoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
