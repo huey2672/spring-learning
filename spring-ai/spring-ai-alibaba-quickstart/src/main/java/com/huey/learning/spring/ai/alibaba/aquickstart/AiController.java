b8fc6201419ba0f233b1bf4d1e6561b40df283c5
huey2672 <huey2672@163.com>
2025-03-17 16:29:19 +0800

    spring ai quick start

---
+++ spring-ai/spring-ai-alibaba-quickstart/src/main/java/com/huey/learning/spring/ai/alibaba/aquickstart/AiController.java
@@ -0,0 +1,53 @@
+package com.huey.learning.spring.ai.alibaba.aquickstart;
+
+import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatOptions;
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ai")
+public class AiController {
+
+    private static final String DEFAULT_PROMPT = "你是一个博学的智能聊天助手，请根据用户提问回答！";
+
+    private final ChatClient dashScopeChatClient;
+
+    public AiController(ChatClient.Builder chatClientBuilder) {
+        this.dashScopeChatClient = chatClientBuilder
+                .defaultSystem(DEFAULT_PROMPT)
+                // 实现 Chat Memory 的 Advisor
+                // 在使用 Chat Memory 时，需要指定对话 ID，以便 Spring AI 处理上下文。
+                .defaultAdvisors(
+                        new MessageChatMemoryAdvisor(new InMemoryChatMemory())
+                )
+                // 实现 Logger 的 Advisor
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor()
+                )
+                // 设置 ChatClient 中 ChatModel 的 Options 参数
+                .defaultOptions(
+                        DashScopeChatOptions.builder()
+                                .withTopP(0.7)
+                                .build()
+                )
+                .build();
+    }
+
+    /**
+     * ChatClient 简单调用
+     */
+    @GetMapping("/simple/chat")
+    public String simpleChat(@RequestParam(value = "query", defaultValue = "你好，很高兴认识你，能简单介绍一下自己吗？") String query) {
+
+        return dashScopeChatClient.prompt(query).call().content();
+    }
+
+}
