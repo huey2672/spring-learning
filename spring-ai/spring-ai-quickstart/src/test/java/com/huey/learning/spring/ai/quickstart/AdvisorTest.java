cd557386acd18a6d67fefd98e5bbb68d9509d306
huey2672 <huey2672@163.com>
2025-05-22 22:17:01 +0800

    spring ai quick start

--- spring-ai/spring-ai-quickstart/src/test/java/com/huey/learning/spring/ai/quickstart/AdvisorTest.java
+++ spring-ai/spring-ai-quickstart/src/test/java/com/huey/learning/spring/ai/quickstart/AdvisorTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+
+import java.util.List;
 
 @Slf4j
 @SpringBootTest
@@ -19,10 +22,24 @@ public class AdvisorTest {
     @Test
     public void testSimpleLoggerAdvisor() {
         ChatClient chatClient = chatClientBuilder.defaultAdvisors(new SimpleLoggerAdvisor()).build();
-        String answer = chatClient.prompt()
-                .user("请介绍一下你自己")
+        List<Book> recommendedBooks = chatClient.prompt()
+                .user(u -> u.text("请推荐{num}本关于{genre}的经典书籍")
+                        .param("num", "5")
+                        .param("genre", "中国古代史"))
                 .call()
-                .content();
-        log.info(answer);
+                .entity(new ParameterizedTypeReference<>() {});
+        log.info("推荐的书籍列表：{}", recommendedBooks);
     }
+
+    @Test
+    public void testMessageChatMemoryAdvisor() {
+//        ChatClient chatClient = chatClientBuilder.defaultAdvisors(new MessageChatMemoryAdvisor()).build();
+//        List<Book> recommendedBooks = chatClient.prompt()
+//                .user(u -> u.text("请推荐{num}本关于{genre}的经典书籍")
+//                        .param("num", "5")
+//                        .param("genre", "中国古代史"))
+//                .call()
+//                .entity(new ParameterizedTypeReference<>() {});
+    }
+
 }
